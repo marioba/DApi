f185abbbd2e253d3e626f96c726492d7522164ce
Mario Baranzini <mario@opengis.ch>
2022-05-09 11:26:03 +0200

    Add dialogId validation in dialog serializer

--- dataapi/core/serializers.py
+++ dataapi/core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from dataapi.core.models import Dialog, Consent
 
@@ -14,6 +15,10 @@ class DialogSerializer(serializers.ModelSerializer):
         model = Dialog
         fields = ['customerId', 'dialogId', 'text', 'language']
 
+    def validate(self, data):
+        if Dialog.objects.filter(dialog_id=data.get('dialog_id')).exists():
+            raise ValidationError('DialogId already exists')
+        return data
 
 class ConsentSerializer(serializers.ModelSerializer):
 
